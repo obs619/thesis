cf9bbd2e5d05dda0360058f1cdfc209879a8820e
laronandrew11 <laronandrew11@yahoo.com>
2014-06-29 17:40:55 +0800

    World creation code started.

--- cardgame/src/com/cardgame/host/Area.java
+++ cardgame/src/com/cardgame/host/Area.java
@@ -3,22 +3,40 @@ package com.cardgame.host;
 import java.util.List;
 
 public class Area {
+	private String name;
 	private List<Area> subAreas;
 	private List<Card> cards;
+	
 	/**
-	 * Players (private screens) which can see the faces of cards in this area.
+	 * Players (private screens) which can see the faces of cards in this area (but not in subareas).
 	 */
 	private List<User> permittedPlayers;
 	/**
-	 * Spectators (public screens) which can see the faces of cards in this area.
+	 * Spectators (public screens) which can see the faces of cards in this area (but not in subareas).
 	 */
 	private List<User> permittedSpectators;
+	
+	public Area(String name)
+	{
+		this.name=name;
+	}
+	
+	public Area(String name, List<User>permittedPlayers,List<User>permittedSpectators)
+	{
+		this.name=name;
+		this.permittedPlayers=permittedPlayers;
+		this.permittedSpectators=permittedSpectators;
+	}
 	
 	public List<Area> getSubAreas() {
 		return subAreas;
 	}
 	public void setSubAreas(List<Area> subAreas) {
 		this.subAreas = subAreas;
+	}
+	public void addSubArea(Area subArea)
+	{
+		subAreas.add(subArea);
 	}
 	public List<Card> getCards() {
 		return cards;
--- cardgame/src/com/cardgame/host/World.java
+++ cardgame/src/com/cardgame/host/World.java
@@ -1,4 +1,7 @@
 package com.cardgame.host;
+
+import java.util.List;
+
 /**
  * This class will handle game logic and pass messages to clients informing them of changes in game state.
  * @author Andrew
@@ -6,6 +9,28 @@ package com.cardgame.host;
  */
 public class World {
 	private Area rootArea;
+	private List<User>players;
+	private List<User>spectators;
+	public World(List<User>players, List<User> spectators)
+	{
+		this.players=players;
+		this.spectators=spectators;
+		rootArea=new Area("rootArea");//not sure if I should pass all these players/spectators
+		Area deck=new Area("deck");//all cards in deck should be face down regardless of who is viewing them
+		Area discardPile=new Area("discardPile",players,spectators);//cards here are face up to everyone
+		//TODO: populate deck and discard pile with cards
+		rootArea.addSubArea(deck);
+		rootArea.addSubArea(discardPile);
+		for(User p: players)
+		{
+			Area newHand=new Area("handOf"+p.getUsername());
+			//TODO: populate hand
+			newHand.addPermittedPlayer(p);
+			rootArea.addSubArea(newHand);
+		}
+		
+		
+	}
 	
 	/**Called when a client sends a message informing the host that it has performed some action, such as drawing or playing a card
 	 * 
