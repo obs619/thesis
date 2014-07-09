ae696fbfd8274a3b1499a9a82f4e6cff18ea5a50
peigen831 <peigen_xu@dlsu.ph>
2014-07-09 11:21:19 +0800

    singleton networkinitializer

--- cardgame/src/com/cardgame/screenapi/NetworkInitializer.java
+++ cardgame/src/com/cardgame/screenapi/NetworkInitializer.java
@@ -1,5 +1,14 @@
 package com.cardgame.screenapi;
 
 public class NetworkInitializer {
-
+	
+	private static NetworkInitializer networkInitializer = null;
+	
+	public static NetworkInitializer getInstance() 
+	{
+	    if(networkInitializer == null) {
+	    	networkInitializer = new NetworkInitializer();
+	      }
+	      return networkInitializer;
+	}
 }
