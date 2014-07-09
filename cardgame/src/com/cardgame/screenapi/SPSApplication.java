bc09f4aa157dd3229190a8224dc69498345c9f8b
peigen831 <peigen_xu@dlsu.ph>
2014-07-09 12:00:39 +0800

    SPSApplication setNetworkInitializer

--- cardgame/src/com/cardgame/screenapi/SPSApplication.java
+++ cardgame/src/com/cardgame/screenapi/SPSApplication.java
@@ -3,5 +3,34 @@ package com.cardgame.screenapi;
 import android.app.Application;
 
 public class SPSApplication extends Application {
+	private Screen screen;
+	private EventManager eventManager;
+	private NetworkInitializer networkInitializer; 
+	
+	public void setNetworkInitializer() {
+		this.networkInitializer = NetworkInitializer.getInstance();
+	}
+	
+	// getters and setters
+	public Screen getScreen() {
+		return screen;
+	}
 
+	public void setScreen(Screen screen) {
+		this.screen = screen;
+	}
+
+	public EventManager getEventManager() {
+		return eventManager;
+	}
+
+	public void setEventManager(EventManager eventManager) {
+		this.eventManager = eventManager;
+	}
+
+	public NetworkInitializer getNetworkInitializer() {
+		return networkInitializer;
+	}
+
+	
 }
