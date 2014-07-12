aeb224dd467134b7574bfc92bdc229755b647d77
obs619 <oliversyson@yahoo.com>
2014-07-12 10:06:47 +0800

    recreated entire cardgame project because of tons of errors from the previous one

---
+++ CardGameApp/src/com/cardgame/activities/LobbyActivity.java
@@ -0,0 +1,35 @@
+package com.cardgame.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cardgame.R;
+
+
+public class LobbyActivity extends Activity {
+	
+	private Button btnPersonal;
+	private Button btnShared;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lobby);
+		btnPersonal = (Button) findViewById(R.id.btnLobbyPersonal);
+		btnShared = (Button) findViewById(R.id.btnLobbyShared);
+	}
+	
+	public void selectAsPersonal(View v) {
+		Intent intent = new Intent(this, PlayPersonalActivity.class);
+		startActivity(intent);
+	}
+	
+	public void selectAsShared(View v) {
+		Intent intent = new Intent(this, PlaySharedActivity.class);
+		startActivity(intent);
+	}
+	
+}
