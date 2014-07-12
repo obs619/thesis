43f5d0310810458415dbe743b73a23f372dc69ae
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-07-12 18:07:39 +0800

    Turned SharedScreen into an actual public screen

--- CardGameApp/src/com/cardgame/activities/PlaySharedActivity.java
+++ CardGameApp/src/com/cardgame/activities/PlaySharedActivity.java
@@ -1,6 +1,8 @@
 package com.cardgame.activities;
 
 import com.cardgame.R;
+import com.cardgame.screenapi.Screen;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -12,9 +14,11 @@ import android.widget.ListView;
  *		selected.
  */
 
-public class PlaySharedActivity extends Activity {
+public class PlaySharedActivity extends Activity implements Screen {
 	
 	private ListView listCards;
+	private boolean isPublic;
+	private String name;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,8 @@ public class PlaySharedActivity extends Activity {
 		setContentView(R.layout.activity_play_shared);
 		
 		listCards = (ListView) findViewById(R.id.listSharedCardsPlayed);
+		isPublic = false;
+		name = null;
 	}
 	
 	/** On update, call this function, feel free to edit
@@ -30,6 +36,31 @@ public class PlaySharedActivity extends Activity {
 	public void update() {
 		// TODO receive card
 		// TODO add card to listview
+	}
+	
+	@Override
+	public boolean isShared() {
+		return isPublic;
+	}
+	
+	@Override
+	public void setAsShared() {
+		isPublic = true;
+	}
+	
+	@Override
+	public void setAsPersonal() {
+		isPublic = false;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
