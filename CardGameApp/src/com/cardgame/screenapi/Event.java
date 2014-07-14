5570408ff2937f8e65ba18ffd910dfc24669ea3b
laronandrew11 <laronandrew11@yahoo.com>
2014-07-14 13:38:57 +0800

    added APIEventHandler.java

--- CardGameApp/src/com/cardgame/screenapi/Event.java
+++ CardGameApp/src/com/cardgame/screenapi/Event.java
@@ -17,6 +17,7 @@ public class Event {//can developer subclass using reflection?
 	public static final int T_LOCK_SESSION=2;
 	public static final int T_UNLOCK_SESSION=3;
 	public static final int T_LEAVE_SESSION=4;
+	public static final int T_JOIN_NETWORK=5;
 	public static final int USER_OWNNODE=10;
 	private String source;//or Screen
 	private String recipient;//who does this event affect?
