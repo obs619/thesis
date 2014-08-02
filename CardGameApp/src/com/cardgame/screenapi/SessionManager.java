423b76aaa4c33f792a4a69e32ae882f6ed00f909
obs619 <oliversyson@yahoo.com>
2014-08-02 15:59:21 +0800

    merged

--- CardGameApp/src/com/cardgame/screenapi/SessionManager.java
+++ CardGameApp/src/com/cardgame/screenapi/SessionManager.java
@@ -3,6 +3,7 @@ package com.cardgame.screenapi;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SessionManager {
 	private String sessionID;//auto-generate
