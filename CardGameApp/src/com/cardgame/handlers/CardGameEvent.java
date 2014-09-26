e5a1d0119a235f29d49a776ae629051c6b6ac73f
obs619 <oliversyson@yahoo.com>
2014-09-26 22:44:44 +0800

    removed unused static ints at cardgameevent.java

--- CardGameApp/src/com/cardgame/handlers/CardGameEvent.java
+++ CardGameApp/src/com/cardgame/handlers/CardGameEvent.java
@@ -7,40 +7,20 @@ public class CardGameEvent extends Event {
 		super(recipient, type, payload);
 	}
 	
-	/**for Monkey Monkey, draw card from adjacent player
-	 * RECIPIENT: player on left; event must be triggered on remote screen before local screen
-	 */
 	public static final int CARD_DRAWN=0;
 	public static final int CARD_PLAYED=1;
-	public static final int REQUEST_WORLD=2;
-	/**
-	 * A delta containing all information about current state of game world. Sent on start of game or if a player desyncs.
-	 */
-	public static final int SEND_WORLDVIEW=4;
 
-	public static final int TURN_OVER=5;
-	/**
-	 * A delta sent to a specific player to inform it of which cards are allowed to be played. One of these should be sent to all players after every turn(?)
-	 */
-	public static final int PLAYABLE_CARDS_CHANGED=6;
+	public static final int START_GAME=2;
+	public static final int DECK_DISTRIBUTE=3;
+	public static final int PLAYER_NUM=4;
+	public static final int ADJACENT_PLAYER=5;
 	
-	public static final int GAME_OVER=7;
-	
-	/**
-	 * A delta sent when the host has reshuffled the cards in the deck/discard pile. Not needed for Monkey Monkey
-	 */
-	public static final int DECK_RESHUFFLED=8;
-	public static final int START_GAME=9;
-	public static final int DECK_DISTRIBUTE=10;
-	public static final int PLAYER_NUM=11;
-	public static final int ADJACENT_PLAYER=12;
-	
-	public static final int DRAW_RESPOND=13;
-	public static final int OUT_OF_CARDS=14;
-	public static final int CHANGE_NUM_PLAYERS=15;
-	public static final int NOTIFY_HOST=16;
-	public static final int NOTIFY_PLAYER_TURN=17;
-	public static final int LOSE_PLAYER=18;
+	public static final int DRAW_RESPOND=6;
+	public static final int OUT_OF_CARDS=7;
+	public static final int CHANGE_NUM_PLAYERS=8;
+	public static final int NOTIFY_HOST=9;
+	public static final int NOTIFY_PLAYER_TURN=10;
+	public static final int LOSE_PLAYER=11;
 	
 	
 }
