0e0517ff5332cac36e19d8776edf5994b2daf4c2
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-10-23 13:29:53 +0800

    Added Javadocs to NetworkManager and factory

--- CardGameApp/src/com/llsx/pps/network/NetworkManager.java
+++ CardGameApp/src/com/llsx/pps/network/NetworkManager.java
@@ -5,12 +5,21 @@ public class NetworkManager {
 	private static NetworkManager instance = null;
 	private static NetworkManagerFactory factory;
 
+	/**
+	 * @return The current instance of <code>NetworkManager</code>
+	 * if it exists; otherwise a new instance of
+	 * <code>NetworkManager</code>.
+	 */
 	public static NetworkManager getInstance() {
 		if(instance == null)
 			instance = factory.createNetworkManager();
 		return instance;
 	}
 	
+	/**
+	 * Sets the given factory as the new default factory.
+	 * @param factory the factory to be used
+	 */
 	public static void setDefaultFactory(NetworkManagerFactory factory) {
 		NetworkManager.factory = factory;
 	}
--- CardGameApp/src/com/llsx/pps/network/NetworkManagerFactory.java
+++ CardGameApp/src/com/llsx/pps/network/NetworkManagerFactory.java
@@ -2,6 +2,10 @@ package com.llsx.pps.network;
 
 public interface NetworkManagerFactory {
 	
+	/**
+	 * Creates and initializes a new <code>NetworkManager</code>.
+	 * @return The initialized <code>NetworkManager</code>.
+	 */
 	public NetworkManager createNetworkManager();
 	
 }
